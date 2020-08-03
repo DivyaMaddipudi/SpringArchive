7ea824c82b223a2835589e09f9b9103533a7dc22
SatyaDivya <divya.maddipudi1@gmail.com>
2020-08-03 11:53:41 +0530

    basic form using spring added

---
+++ 13InputAndRadioInSpring/src/org/divya/MainController.java
@@ -0,0 +1,38 @@
+package org.divya;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.divya.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView("userFormView");
+		User user = new User();
+		modelAndView.addObject("user", user);
+		return modelAndView;
+	}
+	
+	@PostMapping("/displayUserInfo")
+	public ModelAndView displayUserInfo(@ModelAttribute User user) {
+		ModelAndView modelAndView = new ModelAndView("displayUserInfo");
+		System.out.println(user);
+		modelAndView.addObject("user", user);
+		return modelAndView;
+		
+		
+	}
+
+}
