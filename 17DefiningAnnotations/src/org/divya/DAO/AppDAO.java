ce43201d09c26372505f9952eb564f79d0d49851
SatyaDivya <divya.maddipudi1@gmail.com>
2020-08-07 14:02:30 +0530

    annotation based method added'

---
+++ 17DefiningAnnotations/src/org/divya/DAO/AppDAO.java
@@ -0,0 +1,11 @@
+package org.divya.DAO;
+
+import java.util.List;
+
+import org.divya.model.User;
+
+public interface AppDAO {
+	
+	public List<User> listUsers();
+
+}
---
+++ 17DefiningAnnotations/src/org/divya/config/AppConfig.java
@@ -0,0 +1,27 @@
+package org.divya.config;
+
+import javax.sql.DataSource;
+
+import org.divya.DAO.AppDAOImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3303/springdb");
+		dataSource.setUsername("student");
+		dataSource.setPassword("student");
+		
+		return dataSource;
+	}
+	
+	public AppDAOImpl AppDAO() {
+		return new AppDAOImpl(getDataSource());
+	}
+}
