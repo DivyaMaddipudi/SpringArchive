6319f466de666d05001a65303adacc7c0c2c68cd
SatyaDivya <divya.maddipudi1@gmail.com>
2020-08-25 16:27:39 +0530

    Modified status code

--- 21Jax-rsSubResource/src/main/java/org/divya/showroom/resources/Brands.java
+++ 21Jax-rsSubResource/src/main/java/org/divya/showroom/resources/Brands.java
@@ -1,7 +1,9 @@
 package org.divya.showroom.resources;
 
+
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -10,6 +12,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.divya.showroom.hibernate.entity.BrandEntity;
 import org.divya.showroom.services.BrandsService;
@@ -28,9 +32,11 @@ public class Brands {
 	}
 	
 	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	public void postBrands(BrandEntity brand) {
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_HTML)
+	public Response postBrands(BrandEntity brand) {
 		service.addBrand(brand);
+		return Response.status(Status.CREATED).entity("OK").build();
 	}
 
 	@PUT
