a9cbb14c17dec6ac9860549aa0da5f92f2a0dade
SatyaDivya <divya.maddipudi1@gmail.com>
2020-08-31 12:40:12 +0530

    Set JAXRS without web.xml

---
+++ 24NewMavenProject/src/main/java/org/divya/App.java
@@ -0,0 +1,16 @@
+package org.divya;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/demo")
+public class App {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String demo() {
+		return "I love rest";
+	}
+}
