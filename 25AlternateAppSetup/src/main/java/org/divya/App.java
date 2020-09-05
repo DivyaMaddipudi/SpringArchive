c6f282249c79867a564e2c5d0e3c77f9dc579b28
SatyaDivya <divya.maddipudi1@gmail.com>
2020-09-05 13:28:33 +0530

    Singleton objects added

--- 25AlternateAppSetup/src/main/java/org/divya/App.java
+++ 25AlternateAppSetup/src/main/java/org/divya/App.java
@@ -1,16 +1,19 @@
 package org.divya;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/demo")
+@Singleton
 public class App {
 	
+	private int x = 0;
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String demo() {
-		return "I love rest";
+		return "The value of x  is " + (++x);
 	}
 }
