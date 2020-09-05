7e8d995fea52a090e175d773623014855eeb8496
SatyaDivya <divya.maddipudi1@gmail.com>
2020-09-05 15:54:45 +0530

    URL Building in client added

--- 27BuildingClient/src/main/java/org/divya/App.java
+++ 27BuildingClient/src/main/java/org/divya/App.java
@@ -5,6 +5,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -14,10 +16,18 @@ public class App {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String demo() {
+		
 		Client client = ClientBuilder.newClient();
-		String response = client.target("http://localhost:8082/23HATEOAS_1/showroom/brands")
+		WebTarget baseBrandURL = client.target("http://localhost:8082/23HATEOAS_1/showroom/brands");
+		WebTarget brandURL = baseBrandURL.path("{brandId}");
+		
+		
+		
+		String response = brandURL
+							.resolveTemplate("brandId", "6")
 							.request(MediaType.APPLICATION_JSON)
 							.get(String.class);
-		return response;
+		
+	return response;
 	}
 }
