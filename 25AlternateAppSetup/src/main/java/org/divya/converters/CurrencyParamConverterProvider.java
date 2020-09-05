ba841fb956044c6415e993c26fb8f477ce8ebee0
SatyaDivya <divya.maddipudi1@gmail.com>
2020-09-05 14:31:12 +0530

    CurrencyParamConverterProviderModified

--- 25AlternateAppSetup/src/main/java/org/divya/converters/CurrencyParamConverterProvider.java
+++ 25AlternateAppSetup/src/main/java/org/divya/converters/CurrencyParamConverterProvider.java
@@ -7,12 +7,36 @@ import javax.ws.rs.ext.ParamConverter;
 import javax.ws.rs.ext.ParamConverterProvider;
 import javax.ws.rs.ext.Provider;
 
+import org.divya.Currency;
+
 @Provider
 public class CurrencyParamConverterProvider implements ParamConverterProvider {
 
 	@Override
 	public <T> ParamConverter<T> getConverter(Class<T> rawType, Type genericType, Annotation[] annotations) {
-		return (ParamConverter<T>) new CurrencyParamConverter();
+		return new ParamConverter<T>() {
+
+			@Override
+			public T fromString(String code) {
+				
+				Currency currency = null;
+				switch(code.toUpperCase()) {
+				case "EUR": currency = new Currency(code, "Euro"); break;
+				case "USD": currency = new Currency(code, "US Dollar"); break;
+				case "INR": currency = new Currency(code, "Rupees"); break;
+					
+				}
+				
+				return (T) currency;
+
+			}
+
+			@Override
+			public String toString(T currency) {
+				return currency.toString();
+			}
+			
+		};
 	}
 	
 }
