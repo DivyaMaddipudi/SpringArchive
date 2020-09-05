df95458ed743d6a610afe77ad2778e8cdce45e2b
SatyaDivya <divya.maddipudi1@gmail.com>
2020-09-05 16:10:03 +0530

    POST request added in client

--- 27BuildingClient/src/main/java/org/divya/App.java
+++ 27BuildingClient/src/main/java/org/divya/App.java
@@ -1,33 +1,25 @@
 package org.divya;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/app")
+import org.divya.model.Brand;
+
 public class App {
 	
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String demo() {
+	static Client client = ClientBuilder.newClient();
+	static WebTarget baseBrandURL = client.target("http://localhost:8082/23HATEOAS_1/showroom/brands");
+	static WebTarget brandURL = baseBrandURL.path("{brandId}");
+	
+	public static void main(String[] args) {
 		
-		Client client = ClientBuilder.newClient();
-		WebTarget baseBrandURL = client.target("http://localhost:8082/23HATEOAS_1/showroom/brands");
-		WebTarget brandURL = baseBrandURL.path("{brandId}");
-		
-		
-		
-		String response = brandURL
-							.resolveTemplate("brandId", "6")
-							.request(MediaType.APPLICATION_JSON)
-							.get(String.class);
-		
-	return response;
+		Response resonse = baseBrandURL.request(MediaType.APPLICATION_JSON)
+									   .post(Entity.json(new Brand("New Brand name")));
+		System.out.println(resonse.toString());
 	}
 }
---
+++ 27BuildingClient/src/main/java/org/divya/model/Brand.java
@@ -0,0 +1,21 @@
+package org.divya.model;
+
+public class Brand {
+	
+	private String brandName;
+
+	public Brand(String brandName) {
+		super();
+		this.brandName = brandName;
+	}
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+	
+	
+}
