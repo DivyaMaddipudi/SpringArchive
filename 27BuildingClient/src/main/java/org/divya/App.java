5edc150c78a930e63f9cf03907d028ff137be853
SatyaDivya <divya.maddipudi1@gmail.com>
2020-09-05 15:08:32 +0530

    Building client using JAXRS added

---
+++ 27BuildingClient/src/main/java/org/divya/App.java
@@ -0,0 +1,21 @@
+package org.divya;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/app")
+public class App {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String demo() {
+		Client client = ClientBuilder.newClient();
+		Response response = client.target("http://localhost:8082/23HATEOAS_1/showroom/brands/6").request().get();
+		return response.toString();
+	}
+}
